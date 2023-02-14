9de5133ada1bacaecdf4735114491ae9012878e8
mrmaverick01 <122720804+mrmaverick01@users.noreply.github.com>
2023-02-14 08:03:34 +0000

    first commit

---
+++ dom.js
@@ -0,0 +1,16 @@
+// document.dir(document);
+// document.log(document.domain);
+document.log(document.URL);
+document.log(document.title);
+// document.title=123;
+document.log(document.doctype);
+document.log(document.head);
+document.log(document.body);
+document.log(document.all);
+document.log(document.forms);
+document.log(document.all[10]);
+
+
+console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
