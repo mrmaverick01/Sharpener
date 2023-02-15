6efb57886dcf915de38fd5ad25f891c2894abc50
mrmaverick01 <122720804+mrmaverick01@users.noreply.github.com>
2023-02-15 13:10:06 +0000

    4th commit

--- dom.js
+++ dom.js
@@ -18,3 +18,26 @@ console.log(headerTitle);
 var item = document.querySelector('.list-group-item');
 item.style.background = "green";
 document.getElementById("item3").style.display = "none";
+
+////////////////
+var itemList = document.querySelector('#items');
+console.log(itemList.parentNode);
+console.log(itemList.parentElement);
+console.log(itemList.childNodes);
+console.log(itemList.lastElementChild);
+console.log(itemList.firstElementChild);
+console.log(itemList.lastChild);
+console.log(itemList.firstChild);
+console.log(itemList.nextSibling);
+console.log(itemList.nextElementSibling);
+console.log(itemList.previousElementSibling);
+console.log(itemList.previousSibling);
+const newChild = document.createElement("div");
+console.log(newChild);
+newChild.setAttribute('title','Hello');
+var newDiv = document.createTextNode('Hello World');
+newChild.appendChild(newDiv);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+container.insertBefore(newChild,h1);
